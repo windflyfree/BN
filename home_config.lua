6ac7efa32d0561cba0f2507d770aeb2ffbd3f640
windflyfree <22515528@qq.com>
2014-03-17 16:53:10 +0800

    All

--- home_config.lua
+++ home_config.lua
@@ -10,7 +10,7 @@ units = { {cat="critters",page=0,pos=1,num=3},              -- mammoth
 	  
 
 
---[[a_seq={  -- unit location,wp,getskill(what skill，how many mark?)
+a_seq={  -- unit location,wp,getskill(what skill，how many mark?)
     {row=1,col=3,wpx=Skill_x,wpy=getSkill(1,1),drag=false,time=0},   
     {row=1,col=3,wpx=Skill_x,wpy=getSkill(3,2),drag=false,time=0}, 
 	{row=1,col=2,wpx=Skill_x,wpy=getSkill(3,2),drag=false,time=0},
@@ -21,6 +21,5 @@ units = { {cat="critters",page=0,pos=1,num=3},              -- mammoth
 	{row=2,col=2,wpx=Skill_x,wpy=getSkill(1,1),drag=false,time=0}, 
     {row=2,col=4,wpx=Skill_x,wpy=getSkill(1,1),drag=false,time=0}    
 }   
-]]
 
-print(#(units))
+
